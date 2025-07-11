8534f99c7937e4f7c0ddcbdaea066b6d84a1bf43
Jamily Tavares Braga <jamily751@hotmail.com>
2025-07-11 20:10:59 -0300

    enviando desafio bancario

---
+++ variaveis_constantes.py
@@ -0,0 +1,10 @@
+nome = "Jamily"
+
+idade = 22
+
+limite_saque_diario = 1000
+
+#CONSTANTE
+ESTADOS = ["SP", "RJ", "MG", "RS", "SC", "PR", "BA", "PE", "CE", "GO"]
+
+print(nome, idade, limite_saque_diario, ESTADOS)
